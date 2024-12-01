f98ed53ba5cb0a1eb950ef5df99dfbb19fa6e95f
Scott Lai <tuonglai3602@gmail.com>
2024-12-01 11:42:03 -0800

    feat: simplify section z for airline

--- config/nvim/lua/plugins/airline.lua
+++ config/nvim/lua/plugins/airline.lua
@@ -4,9 +4,15 @@ vim.cmd([[
   if !exists('g:airline_symbols')
     let g:airline_symbols = {}
   endif
-  let g:airline_symbols.maxlinenr = ''
-  let g:airline_symbols.branch = ''
+  " let g:airline_symbols.maxlinenr = ''
+  " let g:airline_symbols.branch = ''
 ]])
 
-vim.g["airline#extensions#tabline#enabled"] = 1
-vim.g["airline#extensions#tabline#buffer_nr_show"] = 1
+-- disable nerd icons
+vim.g.airline_symbols_ascii = 1
+
+vim.g['airline#extensions#tabline#enabled'] = 1
+vim.g['airline#extensions#tabline#buffer_nr_show'] = 1
+
+-- Simplify the z section to show only maxline:maxcolumn ≡ [line:column]
+vim.g['airline_section_z'] = '%#__accent_bold#%L:%{col("$")-1} ≡ [%l:%v]%#__restore__#'
