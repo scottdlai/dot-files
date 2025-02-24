0fd49a30040ce2a659b7682be3845caa2240aa7c
Scott Lai <tuonglai3602@gmail.com>
2025-02-23 20:08:45 -0800

    chore: remove nerdcommenter

--- config/nvim/lua/plugins/commeter.lua
+++
@@ -1,6 +0,0 @@
---- NERDCommenter Settings
--- disable nerd default mappings
-vim.g.NERDCreateDefaultMappings = 0
-vim.g.NERDSpaceDelims = 1
-vim.g.NERDToggleCheckAllLines = 1
-vim.g.NERDCompactSexyComs = 1
