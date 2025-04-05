a6aa89d824871707dfa08421ef9079e20ac4e773
Scott Lai <tuonglai3602@gmail.com>
2025-04-05 15:13:01 -0700

    feat: fzf stuffs

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -4,31 +4,118 @@ local function rg_ignore_file_name(opts)
   local fzf_vim_with_preview = vim.fn["fzf#vim#with_preview"]
 
   local args = opts.args
-  local fzf_opts = fzf_vim_with_preview({options = '--prompt "> " --delimiter ":" --nth "4.."'})
-  local command = 'rg --hidden --glob "!.git" --column --line-number --no-heading --color=always --smart-case -- ' .. vim.fn["fzf#shellescape"](args)
+  local prompt = 'Rg> '
+  local header = ':: \x1b[93mCTRL-F\x1b[0m to toggle File name search, \x1b[93mCTRL-R\x1b[0m to re-trigger rg'
+
+  local include_file_name_label = " File name + Text "
+  local exclude_file_name_label = " Text "
+  local delimiter = ':'
+
+  local current_file = vim.fn.expand('%:.')
+
+  local rg = 'rg --hidden --glob "!.git" --column --line-number --no-heading --with-filename --color=always --smart-case -- '
+
+  local nth = "4.."
+  local label = exclude_file_name_label
+  local search_scope = current_file
+
+  -- search file name and all files if query is provided
+  if opts.args ~= '' then
+    nth = "1.."
+    label = include_file_name_label
+    search_scope = '.'
+  end
+
+  local ctrl_f_bind = string.format('ctrl-f:transform-nth([[ $FZF_NTH = "4.." ]] && echo "1.." || echo "4..")+transform-border-label([[ $FZF_NTH = "4.." ]] && echo "%s" || echo "%s")', exclude_file_name_label, include_file_name_label)
+
+  local ctrl_r_bind = string.format('ctrl-r:reload(%s {q})+change-nth(1..)+change-border-label(%s)', rg, include_file_name_label)
+
+  local options = {
+    '--prompt', prompt,
+    '--header', header,
+    '--delimiter', delimiter,
+    '--nth', nth,
+    '--wrap',
+    '--border-label', label,
+    '--bind', ctrl_f_bind,
+    '--bind', ctrl_r_bind,
+    -- clear query after reload rg
+    '--bind', 'load:clear-query',
+  }
+
+  local fzf_opts = fzf_vim_with_preview({options = options})
+
+  local command = rg .. vim.fn["fzf#shellescape"](args) .. ' ' .. search_scope
 
   fzf_vim_grep(command, fzf_opts, opts.bang)
 end
 
-vim.api.nvim_create_user_command("FindAllInProject", rg_ignore_file_name, {
-  bang = true,
-  nargs = "*",
-})
+local function get_files_options()
+  local disable_git_label = " All "
+  local enable_git_label = " Git "
 
--- Alias Jumps to J
-vim.api.nvim_create_user_command('J', 'Jumps', {
-  bang = true,
-  nargs = "*",
-})
+  local header = ':: \x1b[93mCTRL-G\x1b[0m to toggle Git'
 
--- Alias Marks to M
-vim.api.nvim_create_user_command('M', 'Marks', {
-  bang = true,
-  nargs = "*",
-})
+  -- hacky way to include .env files in result if ignored by git
+  local rg_enable_git_command = "rg --files --glob '.env*'; rg --files --hidden --glob '!.git/' --glob '!.env*'"
+  local rg_disable_git_command = "rg --files --hidden --no-ignore --glob '!{.git,node_modules}/' | sort"
+
+  -- https://github.com/junegunn/fzf/blob/master/ADVANCED.md#toggling-with-a-single-key-binding
+  local ctrl_g_bind = string.format('ctrl-g:transform:[[ $FZF_BORDER_LABEL =~ "All" ]] && echo "reload(%s)+change-border-label(%s)" || echo "reload(%s)+change-border-label(%s)"', rg_enable_git_command, enable_git_label, rg_disable_git_command, disable_git_label)
+
+  local start_bind = string.format('start:reload(%s)', rg_enable_git_command)
+
+  local files_options = {
+    '--border-label', enable_git_label,
+    '--scheme', 'path',
+    '--filepath-word',
+    '--header', header,
+    '--bind', ctrl_g_bind,
+    '--bind', start_bind,
+    '--bind', 'ctrl-w:backward-kill-word',
+  }
+
+  return files_options
+end
+
+vim.api.nvim_create_user_command("Rg", rg_ignore_file_name, {
+    bang = true,
+    nargs = "*",
+  })
 
 if os.getenv('TMUX') then
-  vim.g.fzf_layout = { tmux = 'center,60%,60%' }
+  vim.g.fzf_layout = { tmux = 'center,80%,80%' }
 else
-  vim.g.fzf_layout = { window = { width = 0.6, height = 0.6 } }
+  vim.g.fzf_layout = { window = { width = 0.8, height = 0.8 } }
 end
+
+local function get_coc_fzf_opts()
+  local query_top = 'echo \'change-query(!^| )+change-border-label( Top )\''
+  local query_functions = 'echo "change-query([Function] | [Method] | [Constructor] )+change-border-label( Function )"'
+  local query_classes = 'echo "change-query([Class] | [Interface] | [Property] )+change-border-label( Class )"'
+  local query_all = 'echo "clear-query+change-border-label( All )"'
+
+  local toggle_symbols_bind = string.format([=[ctrl-g:transform:if [[ "$FZF_BORDER_LABEL" =~ "All" ]]; then %s; elif [[ "$FZF_BORDER_LABEL" =~ "Top" ]]; then %s; elif [[ "$FZF_BORDER_LABEL" =~ "Function" ]]; then %s; else %s; fi]=], query_top, query_functions, query_classes, query_all)
+
+  return {
+    '--layout', 'reverse',
+    '--border-label', ' All ',
+    '--header', ':: \x1b[93mCTRL-G\x1b[0m to toggle between symbols',
+    -- don't sort the result to preserve outline
+    '--no-sort',
+    '--bind', toggle_symbols_bind,
+    '--bind', 'start:transform:[[ ! "$FZF_PROMPT" =~ "Outline" ]] && echo "change-header()+change-border-label()+clear-query+toggle-sort+unbind(ctrl-g)"',
+  }
+end
+
+vim.g.fzf_vim = {
+  files_options = get_files_options(),
+  windows_options = { '--query', '> ', },
+  buffers_jump = 1,
+  preview_window = { 'right,50%,<80(down,40%)', 'ctrl-/' },
+}
+
+-- COC FZF
+vim.g.coc_fzf_preview_toggle_key = 'ctrl-/'
+vim.g.coc_fzf_preview = vim.g.fzf_vim.preview_window[1]
+vim.g.coc_fzf_opts = get_coc_fzf_opts()
--- config/zsh/fzf-config.zsh
+++ config/zsh/fzf-config.zsh
@@ -6,40 +6,49 @@ source <(fzf --zsh)
 # Scheme author: Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 
-_gen_fzf_default_opts() {
+_gen_fzf_color_scheme() {
+    local color00='#282828'
+    local color01='#3c3836'
+    local color02='#504945'
+    local color03='#665c54'
+    local color04='#bdae93'
+    local color05='#d5c4a1'
+    local color06='#ebdbb2'
+    local color07='#fbf1c7'
+    local color08='#fb4934'
+    local color09='#fe8019'
+    local color0A='#fabd2f'
+    local color0B='#b8bb26'
+    local color0C='#8ec07c'
+    local color0D='#83a598'
+    local color0E='#d3869b'
+    local color0F='#d65d0e'
 
-local color00='#282828'
-local color01='#3c3836'
-local color02='#504945'
-local color03='#665c54'
-local color04='#bdae93'
-local color05='#d5c4a1'
-local color06='#ebdbb2'
-local color07='#fbf1c7'
-local color08='#fb4934'
-local color09='#fe8019'
-local color0A='#fabd2f'
-local color0B='#b8bb26'
-local color0C='#8ec07c'
-local color0D='#83a598'
-local color0E='#d3869b'
-local color0F='#d65d0e'
-
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
-" --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0D"\
-" --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
-
+    echo "--color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D" \
+        "--color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0D" \
+        "--color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
 }
 
-_gen_fzf_default_opts
-
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --style full --layout reverse --tmux center,60%"
+export FZF_DEFAULT_OPTS="$(_gen_fzf_color_scheme) --style full:sharp --layout reverse --tmux center,80%"
 
 export FZF_ALT_C_OPTS="\
-  --walker-root=$HOME \
-  --walker-skip .git,node_modules,build,dist,target,Library \
-  --preview 'ls -AF --color=always {}'"
+    --walker-skip .git,node_modules,build,dist,target,Library \
+    --preview-window 'right,50%,<80(down,40%)' \
+    --bind 'ctrl-/:toggle-preview'
+    --bind 'ctrl-h:transform:echo \"reload:</dev/tty fzf --filter '' --walker dir --walker-root $(realpath ../)\"'
+    --preview 'ls -AF --color=always {}'"
+
+export FZF_CTRL_T_OPTS="\
+    --walker-skip .git,node_modules,build,target,dist \
+    --preview 'fzf-preview.sh {}' \
+    --preview-window 'right,50%,<80(down,40%)' \
+    --bind 'ctrl-/:toggle-preview'"
+
+export FZF_CTRL_R_OPTS="\
+    --wrap \
+    --info inline \
+    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' \
+    --header ':: Press CTRL-Y to copy command into clipboard'"
 
 # Map Ctrl-p to launch fzf cd widget
 bindkey '^p' fzf-cd-widget
