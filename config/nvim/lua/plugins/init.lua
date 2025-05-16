ec0023fc981f8bccfb7252b081583363bf92cb8b
Scott Lai <tuonglai3602@gmail.com>
2025-05-16 08:13:56 -0700

    feat: disable jumping to another tmux pane in nvim when zoom

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -7,4 +7,6 @@ require('plugins/treesitter')
 require('plugins/surround')
 
 vim.g.tmux_navigator_no_mappings = 1 -- disable plugin default keybind
+-- disable navigating to another tmux pane when zoomed
+vim.g.tmux_navigator_disable_when_zoomed = 1
 vim.g.gitgutter_map_keys = 0
