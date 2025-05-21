ffa2bbfa02a7e0d564fd2d40f4c73b072b9d1b01
Scott Lai <tuonglai3602@gmail.com>
2025-05-21 12:44:28 -0700

    feat: add script to update plugin

--- config/zsh/plugin.zsh
+++ config/zsh/plugin.zsh
@@ -1,3 +1,5 @@
+local plugin_root_dir="$XDG_DATA_HOME/zsh/plugins"
+
 function zsh-plugin {
     if [ -z "$1" ]; then
         return
@@ -6,7 +8,7 @@ function zsh-plugin {
     # username/repo -> repo
     local plugin_name=$(cut -d '/' -f2 <<< "$1")
 
-    local plugin_dir="$XDG_DATA_HOME/zsh/plugins/$plugin_name"
+    local plugin_dir="$plugin_root_dir/$plugin_name"
     local script_name="${2:-$plugin_name.zsh}"
 
     if [[ ! -d "$plugin_dir" ]]; then
@@ -16,3 +18,9 @@ function zsh-plugin {
     source "$plugin_dir/$script_name"
 }
 
+function zsh-plugin-update {
+    for plugin in $plugin_root_dir/*; do
+        echo "Updating $(basename $plugin)..."
+        git -C "$plugin" pull
+    done
+}
