3155844c1765a95c1b059bcfa5c7ad2b15ba0f5b
Scott Lai <tuonglai3602@gmail.com>
2025-05-27 11:43:09 -0700

    feat: better highlight for quickscope

--- config/nvim/lua/plugins/quickscope.lua
+++ config/nvim/lua/plugins/quickscope.lua
@@ -1,2 +1,12 @@
 -- only highlight on key instead of automatically
 vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  desc = 'Better highlighting for quick scope',
+  group = vim.api.nvim_create_augroup('quick-scope-color', {}),
+  callback = function()
+    vim.cmd([[highlight! link QuickScopePrimary Search]])
+    vim.cmd([[highlight! link QuickScopeSecondary Search]])
+  end,
+})
