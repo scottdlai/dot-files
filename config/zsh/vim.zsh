bc5814288ac49d72b94e53d8aa8d133a1cb76ff1
Scott Lai <tuonglai3602@gmail.com>
2025-05-29 17:54:06 -0700

    feat: use <C-x><C-e> to edit current command in $EDITOR

--- config/zsh/vim.zsh
+++ config/zsh/vim.zsh
@@ -41,3 +41,8 @@ zle-line-init() {
 zle -N zle-line-init
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
 preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+# https://unix.stackexchange.com/a/34251
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
