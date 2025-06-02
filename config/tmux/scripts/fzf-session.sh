b57e159beff8abd42face14df01cc3482871f945
Scott Lai <tuonglai3602@gmail.com>
2025-06-02 12:01:43 -0700

    feat: make tmux session full screen

--- config/tmux/scripts/fzf-session.sh
+++ config/tmux/scripts/fzf-session.sh
@@ -1,14 +1,19 @@
 function main {
     local current_session="$(tmux display-message -p '#S')"
-    # filter out current session
-    local list_sessions="echo $current_session; tmux list-sessions -F '#S' -f '#{!=:#S,'$current_session'}'";
+
+    # put current session first
+    local list_sessions="echo \"$current_session\"; tmux list-sessions -F '#S' -f '#{!=:#S,'$current_session'}' 2>&1";
 
     fzf --bind "ctrl-x:execute-silent(tmux kill-session -t {})+reload:$list_sessions" \
         --bind "start:reload:$list_sessions" \
+        --bind "focus:transform-preview-label:echo ' {} '" \
         --bind 'ctrl-/:toggle-preview' \
-        --preview 'tmux list-windows -F "#{window_index}: #W#F (#{window_panes} panes)" -t {}' \
-        --preview-window 'right,50%,<60(down,40%)' \
-        --preview-label ' Windows ' \
+        --preview 'tmux capture-pane -e -pt {}' \
+        --preview-window 'down,60%' \
+        --prompt 'switch-client ' \
+        --style default \
+        --border none \
+        --tmux center,100% \
         --header-lines 1 |
         xargs -I '{}' tmux switch-client -t '{}'
 }
